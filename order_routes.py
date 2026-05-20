33db2edef83738a787c81be59d095fcea2755b62
crisocean <crisplusz01@gmail.com>
2026-05-20 02:08:20 -0300

    feat: rota de listar produtos feita

--- order_routes.py
+++ order_routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
+from database import consulta_produtos
 
 order_router = APIRouter(prefix="/orders", tags=["produtos"])
 
 @order_router.get("/")
 
-async def produtos():
+async def listar_produtos():
+    produtos = consulta_produtos()
     """Rota de produtos"""
-    return {"message": "Você acessou a rota de produtos", "autenticado": False}
+    return {"message": "Você acessou a rota de produtos", "autenticado": False, "produtos": produtos}
