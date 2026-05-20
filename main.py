5b65232c6df3c1e8e1c44d0cea284ec6d43d9ff4
crisocean <crisplusz01@gmail.com>
2026-05-20 02:05:45 -0300

    rota de home implementada

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-
-app = FastAPI()
 from auth_routes import auth_router #roteador de rotas de autenticação
 from order_routes import order_router # roteador de rotas de pedidos
 
+app = FastAPI()
+#só incluindo as rotas
 app.include_router(auth_router) # inclui as rotas de autenticação
 app.include_router(order_router) # inclui as rotas de pedidos
+
+@app.get("/")
+async def home ():
+    return {"message": "Bem-vindo à API de gerenciamento de estoque!"}
