0682f03cd6dc960711403365281e72e40582a516
crisocean <crisplusz01@gmail.com>
2026-06-20 13:15:16 -0300

    rotas de out e delete adicionadas

--- lojas_router.py
+++ lojas_router.py
@@ -39,3 +39,26 @@ def listar_lojas():
     sql = "SELECT id_loja, nome_loja, cidade, status_loja FROM lojas WHERE status_loja = 'ativa';"
     lojas_listadas = execute_query(sql)
     return {"status": "sucesso", "lojas": lojas_listadas}
+
+@loja_router.put("/{id_loja}")
+def atualizar_loja(id_loja: int, loja: LojaNova):
+    sql = """
+        UPDATE lojas
+        SET id_empresa = %s, nome_loja = %s, endereco_loja = %s,
+        cidade = %s, estado = %s, telefone = %s, status_loja = %s
+        WHERE id_loja = %s;
+    """
+    valores = (
+        loja.id_empresa, loja.nome_loja, loja.endereco_loja,
+        loja.cidade, loja.estado, loja.telefone, loja.status_loja,
+        id_loja
+    )
+    execute_query(sql, valores)
+    return {"status": "sucesso", "mensagem": f"Loja {loja.nome_loja} atualizada com sucesso"}
+    
+@loja_router.delete("/{id_loja}")
+def inativar_loja(id_loja: int):
+    sql = "UPDATE lojas SET status_loja = 'inativa' WHERE id_loja = %s;"
+    execute_query(sql, (id_loja,))
+    return {"status": "sucesso", "mensagem": f"Loja com ID {id_loja} foi inativada com sucesso"}
+    
